31d25f8e822cafde37693de96910887278b57995
Bhairav Mehta <bhairavmehta95@gmail.com>
2019-05-09 11:53:02 -0400

    initial

---
+++ envs/heuristics/lunar/heuristics.py
@@ -0,0 +1,55 @@
+def heuristic(env, s):
+    # Heuristic for:
+    # 1. Testing. 
+    # 2. Demonstration rollout.
+    angle_targ = s[0]*0.5 + s[2]*1.0         # angle should point towards center (s[0] is horizontal coordinate, s[2] hor speed)
+    if angle_targ >  0.4: angle_targ =  0.4  # more than 0.4 radians (22 degrees) is bad
+    if angle_targ < -0.4: angle_targ = -0.4
+    hover_targ = 0.55*np.abs(s[0])           # target y should be proporional to horizontal offset
+
+    # PID controller: s[4] angle, s[5] angularSpeed
+    angle_todo = (angle_targ - s[4])*0.5 - (s[5])*1.0
+    #print("angle_targ=%0.2f, angle_todo=%0.2f" % (angle_targ, angle_todo))
+
+    # PID controller: s[1] vertical coordinate s[3] vertical speed
+    hover_todo = (hover_targ - s[1])*0.5 - (s[3])*0.5
+    #print("hover_targ=%0.2f, hover_todo=%0.2f" % (hover_targ, hover_todo))
+
+    if s[6] or s[7]: # legs have contact
+        angle_todo = 0
+        hover_todo = -(s[3])*0.5  # override to reduce fall speed, that's all we need after contact
+
+    if env.continuous:
+        a = np.array( [hover_todo*20 - 1, -angle_todo*20] )
+        a = np.clip(a, -1, +1)
+    else:
+        a = 0
+        if hover_todo > np.abs(angle_todo) and hover_todo > 0.05: a = 2
+        elif angle_todo < -0.05: a = 3
+        elif angle_todo > +0.05: a = 1
+    return a
+
+def uncalibrated(env, s):
+    # Heuristic for:
+    # 1. Testing. 
+    # 2. Demonstration rollout.
+    angle_targ = s[0]*0.4 + s[2]*0.9         # angle should point towards center (s[0] is horizontal coordinate, s[2] hor speed)
+    if angle_targ >  0.5: angle_targ =  0.5  # more than 0.4 radians (22 degrees) is bad
+    if angle_targ < -0.5: angle_targ = -0.5
+    hover_targ = 0.8*np.abs(s[0])           # target y should be proporional to horizontal offset
+
+    # PID controller: s[4] angle, s[5] angularSpeed
+    angle_todo = (angle_targ - s[4])*1. - (s[5])*0.2
+    #print("angle_targ=%0.2f, angle_todo=%0.2f" % (angle_targ, angle_todo))
+
+    # PID controller: s[1] vertical coordinate s[3] vertical speed
+    hover_todo = (hover_targ - s[1])*0.25 - (s[3])*0.25
+    #print("hover_targ=%0.2f, hover_todo=%0.2f" % (hover_targ, hover_todo))
+
+    if s[6] or s[7]: # legs have contact
+        angle_todo = 0
+        hover_todo = -(s[3])*0.5  # override to reduce fall speed, that's all we need after contact
+
+    a = np.array( [hover_todo*20 - 1, -angle_todo*20] )
+    a = np.clip(a, -1, +1)
+    return a
---
+++ policies/simple.py
@@ -0,0 +1,157 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Categorical
+
+eps = np.finfo(np.float32).eps.item()
+
+# Normal
+FixedNormal = torch.distributions.Normal
+
+log_prob_normal = FixedNormal.log_prob
+FixedNormal.log_probs = lambda self, actions: log_prob_normal(self, actions).sum(-1, keepdim=True)
+
+normal_entropy = FixedNormal.entropy
+FixedNormal.entropy = lambda self: normal_entropy(self).sum(-1)
+
+FixedNormal.mode = lambda self: self.mean
+
+def init(module, weight_init, bias_init, gain=1):
+    weight_init(module.weight.data, gain=gain)
+    bias_init(module.bias.data)
+    return module
+
+class AddBias(nn.Module):
+    def __init__(self, bias):
+        super(AddBias, self).__init__()
+        self._bias = nn.Parameter(bias.unsqueeze(1))
+
+    def forward(self, x):
+        if x.dim() == 2:
+            bias = self._bias.t().view(1, -1)
+        else:
+            bias = self._bias.t().view(1, -1, 1, 1)
+
+        return x + bias
+
+class GaussianPolicy(nn.Module):
+    def __init__(self, num_inputs, num_outputs):
+        super(GaussianPolicy, self).__init__()
+
+        init_ = lambda m: init(m,
+            nn.init.orthogonal_,
+            lambda x: nn.init.constant_(x, 0))
+
+        self.fc_mean = init_(nn.Linear(num_inputs, num_outputs))
+        self.logstd = AddBias(torch.zeros(num_outputs))
+
+        self.saved_log_probs = []
+        self.rewards = []
+
+    def forward(self, x):
+        action_mean = self.fc_mean(x)
+
+        #  An ugly hack for my KFAC implementation.
+        zeros = torch.zeros(action_mean.size())
+        if x.is_cuda:
+            zeros = zeros.cuda()
+
+        action_logstd = self.logstd(zeros)
+        return FixedNormal(action_mean, action_logstd.exp())
+
+class CategoricalPolicy(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(CategoricalPolicy, self).__init__()
+        self.affine1 = nn.Linear(state_dim, action_dim)
+
+        self.saved_log_probs = []
+        self.rewards = []
+
+    def forward(self, x):
+        action_scores = self.affine1(x)
+        return F.softmax(action_scores, dim=1)
+
+class AgentPolicy:
+    def __init__(self, state_dim=8, action_dim=2):
+        self.policy = GaussianPolicy(state_dim, action_dim)
+        self.optimizer = optim.Adam(self.policy.parameters(), lr=1e-2)
+
+    def select_action(self, state, deterministic=False, save_log_probs=True):
+        state = torch.from_numpy(state).float().unsqueeze(0)
+        probs = self.policy(state)
+        # m = Categorical(probs)
+
+        if deterministic:
+            action = probs.mode()
+        else:
+            action = probs.sample()
+       
+        if save_log_probs:
+            self.policy.saved_log_probs.append(probs.log_prob(action))
+
+        return action.cpu().data.numpy()[0]
+
+    def log(self, reward):
+        self.policy.rewards.append(reward)
+
+    def load_from_file(self, file):
+        self.policy.load_state_dict(torch.load(file))
+
+    def load_from_policy(self, original):
+        with torch.no_grad():
+            for param, target_param in zip(self.policy.parameters(), original.policy.parameters()):
+                param.data.copy_(target_param.data)
+                param.requires_grad = False
+    
+    def perturb(self, alpha, weight_noise, bias_noise):
+        with torch.no_grad():
+            weight_noise = torch.from_numpy(alpha * weight_noise).float()
+            bias_noise = torch.from_numpy(alpha * bias_noise).float()
+            for param, noise in zip(self.policy.parameters(), [weight_noise, bias_noise]):
+                param.add_(noise)
+                param.requires_grad = False
+
+    def extract_gradients(self, gamma):
+        R = 0
+        policy_loss = []
+        rewards = []
+        for r in self.policy.rewards[::-1]:
+            R = r + gamma * R
+            rewards.insert(0, R)
+        rewards = torch.tensor(rewards)
+        rewards = (rewards - rewards.mean()) / (rewards.std() + eps)
+        for log_prob, reward in zip(self.policy.saved_log_probs, rewards):
+            policy_loss.append(-log_prob * reward)
+        self.optimizer.zero_grad()
+        policy_loss = torch.cat(policy_loss).sum()
+        policy_loss.backward()
+
+        gradients = []
+        for x in self.policy.parameters():
+            gradients.append(np.array(x.grad))
+
+        del self.policy.rewards[:]
+        del self.policy.saved_log_probs[:]
+        self.optimizer.zero_grad()
+
+        return np.column_stack(gradients).ravel()
+
+    def finish_episode(self, gamma):
+        R = 0
+        policy_loss = []
+        rewards = []
+        for r in self.policy.rewards[::-1]:
+            R = r + gamma * R
+            rewards.insert(0, R)
+        rewards = torch.tensor(rewards)
+        rewards = (rewards - rewards.mean()) / (rewards.std() + eps)
+        for log_prob, reward in zip(self.policy.saved_log_probs, rewards):
+            policy_loss.append(-log_prob * reward)
+        self.optimizer.zero_grad()
+        policy_loss = torch.cat(policy_loss).sum()
+        policy_loss.backward()
+        self.optimizer.step()
+        del self.policy.rewards[:]
+        del self.policy.saved_log_probs[:]
