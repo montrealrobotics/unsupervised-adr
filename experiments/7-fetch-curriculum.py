3108163aebc0a572d59ea13de21de78c46990848
SharathRaparthy <40888673+SharathRaparthy@users.noreply.github.com>
2019-07-17 15:34:20 -0400

    done changes

--- experiments/7-fetch-curriculum.py
+++ experiments/7-fetch-curriculum.py
@@ -19,26 +19,25 @@ parser = argparse.ArgumentParser(description="Experiments on Fetch")
 parser.add_argument('--env-name', type=str, default='ResidualSlipperyPush-v0')
 parser.add_argument('--seed', type=int, default=-1, metavar='N', help='random seed')
 
+env = gym.make(args.env_name)
+obs = env.reset()
+
+STATE_DIM = obs["observation"].shape[0]
+GOAL_DIM = obs["achieved_goal"].shape[0]
+TAU = 0.01
+
 def experiment(args):
-
-    env = gym.make(args.env_name)
     env.seed(args.seed)
-    obs = env.reset()
-    STATE_DIM = obs["observation"].shape[0]
-    GOAL_DIM = obs["achieved_goal"].shape[0]
-    TAU = 0.01
-
-
-
-    alice_policy = AlicePolicyFetch(goal_dim=GOAL_DIM)
+    
     max_timesteps = 100
-
     total_episodes = 50000
     timesteps = 0
     nepisodes = 0
     nselfplay = 0
     ntarget = 0
     best_reward = -np.inf
+    
+    alice_policy = AlicePolicyFetch(goal_dim=GOAL_DIM)
 
     #Training Loop
     while ntarget < total_episodes:
@@ -48,15 +47,14 @@ def experiment(args):
         alice_done = False
         alice_time = 0
         bobs_goal_state = None
-
-
+        
         #Alice Stopping Policy
-
         while not alice_done and (alice_time < max_timesteps):
             action = alice_policy.select_action(alice_state)
             obs, reward, done, _ = env.step(action)
-
-            # When should I stop the alice policy? Is this correct?
+            
+            #Stopping Criteria
+            if action > np.random.random(): alice_done = True 
             alice_done = done or alice_time + 1 == max_timesteps
             if alice_done== False:
                 alice_state[GOAL_DIM:] = obs["achieved_goal"]
