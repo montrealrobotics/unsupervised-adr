823e550f5384eb54730c8c772d398ff35b2ccecb
SharathRaparthy <sharathchandra.raparthy@gmail.com>
2019-07-20 21:31:00 -0400

    fixed some bugs

--- experiments/7-fetch-curriculum.py
+++ experiments/7-fetch-curriculum.py
@@ -1,26 +1,28 @@
 import argparse
 import gym
 import numpy as np
-from envs import *
-from envs.wrappers import RandomizedEnvWrapper
-from envs.randomized_vecenv import make_vec_envs
+# from envs import *
+# from envs.wrappers import RandomizedEnvWrapper
+# from envs.randomized_vecenv import make_vec_envs
 from policies.simple import BobPolicy, AlicePolicyFetch
-from envs.heuristics.lunar.heuristics import heuristic, uncalibrated
-import os
-import psutil
+# from envs.heuristics.lunar.heuristics import heuristic, uncalibrated
+# import os
+# import psutil
 
 
 parser = argparse.ArgumentParser(description="Experiments on Fetch")
-parser.add_argument('--env-name', type=str, default='ResidualSlipperyPush-v0')
-parser.add_argument('--seed', type=int, default=-1, metavar='N', help='random seed')
+parser.add_argument('--env-name', type=str, default='FetchPush-v1')
+parser.add_argument('--seed', type=int, default=1, metavar='N', help='random seed')
 parser.add_argument('--polyak', type=int, default=0.05, help='Polyak Averaging Coefficient')
 parser.add_argument('--sp-gamma', type=int, default=0.1, help='Self play gamma')
 parser.add_argument('--sp-percent', type=int, default=0.1, help='Self Play Percentage')
+args = parser.parse_args()
 
 env = gym.make(args.env_name)
 obs = env.reset()
 STATE_DIM = obs["observation"].shape[0]
 GOAL_DIM = obs["achieved_goal"].shape[0]
+print(f'Action space : {env.action_space.shape[0]}')
 
 
 def soft_update(target, source):
@@ -35,7 +37,7 @@ def check_closeness(state, goal):
 
 def experiment(args):
     env.seed(args.seed)
-    
+
     max_timesteps = 100
     total_episodes = 50000
     timesteps = 0
@@ -43,14 +45,15 @@ def experiment(args):
     nselfplay = 0
     ntarget = 0
     best_reward = -np.inf
-    
-    alice_policy = AlicePolicyFetch(goal_dim=GOAL_DIM)
-    alice_action_policy = BobPolicy(goal_state=GOAL_DIM)
-    bob_policy = BobPolicy(goal_state=GOAL_DIM)
+
+    alice_policy = AlicePolicyFetch(args, goal_dim=GOAL_DIM*2, action_dim=1)
+    alice_action_policy = BobPolicy(state_dim=GOAL_DIM*2, action_dim=1)
+    bob_policy = BobPolicy(state_dim=GOAL_DIM*2, action_dim=1)
     alice_action_policy.load_from_policy(bob_policy)
 
     # Training Loop
     while ntarget < total_episodes:
+
         obs = env.reset()
         goal_state = obs["achieved_goal"]
         alice_state = np.concatenate([goal_state, np.zeros(GOAL_DIM)])
@@ -67,8 +70,8 @@ def experiment(args):
 
                 # Stopping Criteria
                 if alice_signal > np.random.random(): alice_done = True
-                alice_done = alice_done or env_done or alice_time + 1 == max_timesteps
-                if alice_done == False:
+                alice_done = env_done or alice_time + 1 == max_timesteps or alice_signal and alice_time >= 1
+                if not alice_done:
                     alice_state[GOAL_DIM:] = obs["achieved_goal"]
                     bobs_goal_state = obs["achieved_goal"]
                     alice_time += 1
@@ -76,7 +79,7 @@ def experiment(args):
 
             # Bob's policy
             obs = env.reset()
-            state = obs["observation"]
+            state = obs["achieved_goal"]
             bob_state = np.concatenate([state, bobs_goal_state])
             bob_done = False
             bob_time = 0
@@ -86,7 +89,7 @@ def experiment(args):
                 obs, reward, env_done, _ = env.step(action)
                 bob_signal = check_closeness(obs["achieved_goal"], bobs_goal_state)
                 bob_done = env_done or bob_signal
-                
+
                 if not bob_done:
                     bob_state[:GOAL_DIM] = obs["achieved_goal"]
                     bob_policy.log(0.0)
@@ -99,14 +102,13 @@ def experiment(args):
             bob_policy.finish_episode(gamma=0.99)
         else:
             obs = env.reset()
-            bob_state = np.concatenate([obs["achieved_goal"], np.zeros(GOAL_DIM)])
+            bob_state = np.concatenate([obs["achieved_goal"], np.zeros(GOAL_DIM)], axis=0)
             done = False
 
             while not done:
                 action = bob_policy.select_action(bob_state)
-
                 state, reward, done, _ = env.step(action)
-                bob_state[:STATE_DIM] = state
+                bob_state[:GOAL_DIM] = state["achieved_goal"]
                 bob_policy.log(reward)
                 timesteps += 1
 
@@ -117,3 +119,6 @@ def experiment(args):
             # Soft-Update
             soft_update(alice_action_policy, bob_policy)
 
+
+if __name__ == '__main__':
+    experiment(args)
