6ff3d26a398be3d0c4fef093c02e2f79cdca0d2f
SharathRaparthy <sharathchandra.raparthy@gmail.com>
2019-07-23 12:04:24 -0400

    shapes of arrays

--- hindsight-experience-replay/ddpg_agent.py
+++ hindsight-experience-replay/ddpg_agent.py
@@ -6,17 +6,16 @@ import numpy as np
 from mpi4py import MPI
 from models import actor, critic
 from utils import sync_networks, sync_grads
-from replay_buffer import replay_buffer, ReplayBufferSelfPlay
+from replay_buffer import replay_buffer
 from normalizer import normalizer
 from her import her_sampler
 from policies.simple import AlicePolicyFetch
 
-
-
 """
 ddpg with HER (MPI-version)
-
 """
+
+
 class ddpg_agent:
     def __init__(self, args, env, env_params):
         self.args = args
@@ -48,7 +47,6 @@ class ddpg_agent:
         self.her_module = her_sampler(self.args.replay_strategy, self.args.replay_k, self.env.compute_reward)
         # create the replay buffer
         self.buffer = replay_buffer(self.env_params, self.args.buffer_size, self.her_module.sample_her_transitions)
-        self.replay_buffer = ReplayBufferSelfPlay(capacity=int(1e6))
         # create the normalizer
         self.o_norm = normalizer(size=env_params['obs'], default_clip_range=self.args.clip_range)
         self.g_norm = normalizer(size=env_params['goal'], default_clip_range=self.args.clip_range)
@@ -65,14 +63,10 @@ class ddpg_agent:
             if not os.path.exists(self.model_path):
                 os.makedirs(self.model_path)
 
-
-
     def learn(self):
         """
         train the network
-
         """
-
         evals = []
 
         # start to collect samples
@@ -90,90 +84,100 @@ class ddpg_agent:
                     g = observation['desired_goal']
                     # start to collect samples
                     for t in range(self.env_params['max_timesteps']):
-                        # if np.random.random() < self.args.sp_percent:
-                        #     alice_done = False
-                        #     alice_time = 0
-                        #     # reset the environment
-                        #     obs_alice = self.env.reset()
-                        #     ag = obs_alice['achieved_goal']
-                        #     g = obs_alice['desired_goal']
-                        #     goal_state = ag
-                        #     alice_state = np.concatenate([goal_state, np.zeros(self.env_params["goal"])])
-                        #     # Alice Stopping Policy
-                        #     while not alice_done and (alice_time < self.env_params['max_timesteps']):
-                        #         with torch.no_grad():
-                        #             input_tensor = self._preproc_inputs(obs_alice["observation"], g)
-                        #             pi = self.actor_target_network(input_tensor)
-                        #             action = self._select_actions(pi)
-                        #         obs, reward, env_done, _ = self.env.step(action)
-                        #         alice_signal = self.alice_policy.select_action(alice_state)
-                        #
-                        #         # Stopping Criteria
-                        #         if alice_signal > np.random.random(): alice_done = True
-                        #         alice_done = env_done or alice_time + 1 == self.env_params['max_timesteps'] or alice_signal and alice_time >= 1
-                        #         if not alice_done:
-                        #             alice_state[self.env_params["goal"]:] = obs_alice["achieved_goal"]
-                        #             bobs_goal_state = obs_alice["achieved_goal"]
-                        #             alice_time += 1
-                        #             self.alice_policy.log(0.0)
-                        #
-                        #     # Bob's policy
-                        #     obs = self.env.reset()
-                        #     state = obs["achieved_goal"]
-                        #     obs = obs["observation"]
-                        #     bob_state = np.concatenate([state, bobs_goal_state])
-                        #     bob_done = False
-                        #     bob_time = 0
-                        #
-                        #     while not bob_done and alice_time + bob_time < self.env_params['max_timesteps']:
-                        #         with torch.no_grad():
-                        #             input_tensor = self._preproc_inputs(obs, g)
-                        #             pi = self.actor_network(input_tensor)
-                        #             action = self._select_actions(pi)
-                        #         observation_new, reward, env_done, _ = self.env.step(action)
-                        #         bob_signal = self._check_closeness(observation_new["achieved_goal"], bobs_goal_state)
-                        #         obs_new = observation_new['observation']
-                        #         ag_new = observation_new['achieved_goal']
-                        #         bob_done = env_done or bob_signal
-                        #
-                        #         if not bob_done:
-                        #             bob_state[:self.env_params["goal"]] = ag_new
-                        #             bob_time += 1
-                        #             ep_obs.append(obs.copy())
-                        #             ep_ag.append(ag.copy())
-                        #             ep_g.append(g.copy())
-                        #             ep_actions.append(action.copy())
-                        #             # re-assign the observation
-                        #             obs = obs_new
-                        #             ag = ag_new
-                        # else:
-                        with torch.no_grad():
-                            input_tensor = self._preproc_inputs(obs, g)
-                            pi = self.actor_network(input_tensor)
-                            action = self._select_actions(pi)
-                        # feed the actions into the environment
-                        observation_new, _, _, info = self.env.step(action)
-                        obs_new = observation_new['observation']
-                        ag_new = observation_new['achieved_goal']
-                        # append rollouts
-                        ep_obs.append(obs.copy())
-                        ep_ag.append(ag.copy())
-                        ep_g.append(g.copy())
-                        ep_actions.append(action.copy())
-                        # re-assign the observation
-                        obs = obs_new
-                        ag = ag_new
+                        if np.random.random() < self.args.sp_percent:
+                            alice_done = False
+                            alice_time = 0
+                            alice_state = np.concatenate([ag, np.zeros(self.env_params["goal"])])
+                            # Alice Stopping Policy
+                            while not alice_done and (alice_time < self.env_params['max_timesteps']):
+                                with torch.no_grad():
+                                    input_tensor = self._preproc_inputs(obs, g)
+                                    pi = self.actor_target_network(input_tensor)
+                                    action = self._select_actions(pi)
+                                observation_new, reward, env_done, _ = self.env.step(action)
+                                obs_new = observation_new['observation']
+                                ag_new = observation_new['achieved_goal']
+                                alice_signal = self.alice_policy.select_action(alice_state)
+
+                                # Stopping Criteria
+                                if alice_signal > np.random.random(): alice_done = True
+                                alice_done = env_done or alice_time + 1 == self.env_params['max_timesteps'] or alice_signal and alice_time >= 1
+                                if not alice_done:
+                                    alice_state[self.env_params["goal"]:] = ag_new
+                                    bobs_goal_state = ag_new
+                                    alice_time += 1
+                                    self.alice_policy.log(0.0)
+                                    obs = obs_new
+                                    ag = ag_new
+
+                            # Bob's policy
+                            observation = self.env.reset()
+                            obs = observation['observation']
+                            ag = observation['achieved_goal']
+                            g = observation['desired_goal']
+                            bob_state = np.concatenate([obs, bobs_goal_state])
+                            bob_done = False
+                            bob_time = 0
+
+                            while not bob_done and alice_time + bob_time < self.env_params['max_timesteps']:
+                                with torch.no_grad():
+                                    input_tensor = self._preproc_inputs(obs, g)
+                                    pi = self.actor_network(input_tensor)
+                                    action = self._select_actions(pi)
+                                observation_new, reward, env_done, _ = self.env.step(action)
+                                bob_signal = self._check_closeness(observation_new["achieved_goal"], bobs_goal_state)
+                                obs_new = observation_new['observation']
+                                ag_new = observation_new['achieved_goal']
+                                bob_done = env_done or bob_signal
+
+                                if not bob_done:
+                                    bob_state[:self.env_params["goal"]] = ag_new
+                                    bob_time += 1
+                                    ep_obs.append(obs.copy())
+                                    ep_ag.append(ag.copy())
+                                    ep_g.append(g.copy)
+                                    ep_actions.append(action.copy())
+                                    # re-assign the observation
+                                    obs = obs_new
+                                    ag = ag_new
+                                ep_obs.append(obs.copy())
+                                ep_ag.append(ag.copy())
+
+
+# change the replay_buffer code. Make it dynamic. Also look how you are appending the observartions etc during self play.
+                        else:
+                            with torch.no_grad():
+                                input_tensor = self._preproc_inputs(obs, g)
+                                pi = self.actor_network(input_tensor)
+                                action = self._select_actions(pi)
+                            # feed the actions into the environment
+                            observation_new, _, _, info = self.env.step(action)
+                            obs_new = observation_new['observation']
+                            ag_new = observation_new['achieved_goal']
+                            # append rollouts
+                            ep_obs.append(obs.copy())
+                            ep_ag.append(ag.copy())
+                            ep_g.append(g.copy())
+                            ep_actions.append(action.copy())
+                            # re-assign the observation
+                            obs = obs_new
+                            ag = ag_new
                     ep_obs.append(obs.copy())
                     ep_ag.append(ag.copy())
                     mb_obs.append(ep_obs)
                     mb_ag.append(ep_ag)
                     mb_g.append(ep_g)
                     mb_actions.append(ep_actions)
-                    # convert them into arrays
+                # convert them into arrays
                 mb_obs = np.array(mb_obs)
                 mb_ag = np.array(mb_ag)
                 mb_g = np.array(mb_g)
                 mb_actions = np.array(mb_actions)
+                # printing the shapes
+                print(f'The Shape of mb_obs : {mb_obs.shape}') # The Shape of mb_obs : (10, 51, 10)
+                print(f'The Shape of mb_g: {mb_g.shape}') # The Shape of mb_g: (10, 50, 3)
+                print(f'The Shape of mb_ag : {mb_ag.shape}') # The Shape of mb_ag : (10, 51, 3)
+                print(f'The Shape of mb_actions: {mb_actions.shape}') # The Shape of mb_actions: (10, 50, 4)
                 # store the episodes
                 self.buffer.store_episode([mb_obs, mb_ag, mb_g, mb_actions])
                 self._update_normalizer([mb_obs, mb_ag, mb_g, mb_actions])
@@ -183,15 +187,15 @@ class ddpg_agent:
                 # soft update
                 self._soft_update_target_network(self.actor_target_network, self.actor_network)
                 self._soft_update_target_network(self.critic_target_network, self.critic_network)
-
             # start to do the evaluation
             success_rate = self._eval_agent()
             if MPI.COMM_WORLD.Get_rank() == 0:
                 print('[{}] epoch is: {}, eval success rate is: {:.3f}'.format(datetime.now(), epoch, success_rate))
                 evals.append(success_rate)
                 np.save(osp.join(self.model_path, 'evals.npy'), evals)
-                torch.save([self.o_norm.mean, self.o_norm.std, self.g_norm.mean, self.g_norm.std, self.actor_network.state_dict()], \
-                            self.model_path + '/model.pt')
+                torch.save([self.o_norm.mean, self.o_norm.std, self.g_norm.mean, self.g_norm.std,
+                            self.actor_network.state_dict()], \
+                           self.model_path + '/model.pt')
 
     # pre_process the inputs
     def _preproc_inputs(self, obs, g):
@@ -203,7 +207,7 @@ class ddpg_agent:
         if self.args.cuda:
             inputs = inputs.cuda()
         return inputs
-    
+
     # this function will choose action for the agent and do the exploration
     def _select_actions(self, pi):
         action = pi.cpu().numpy().squeeze()
@@ -212,7 +216,7 @@ class ddpg_agent:
         action = np.clip(action, -self.env_params['action_max'], self.env_params['action_max'])
         # random actions...
         random_actions = np.random.uniform(low=-self.env_params['action_max'], high=self.env_params['action_max'], \
-                                            size=self.env_params['action'])
+                                           size=self.env_params['action'])
         # choose if use the random actions
         action += np.random.binomial(1, self.args.random_eps, 1)[0] * (random_actions - action)
         return action
@@ -225,15 +229,17 @@ class ddpg_agent:
         # get the number of normalization transitions
         num_transitions = mb_actions.shape[1]
         # create the new buffer to store them
-        buffer_temp = {'obs': mb_obs, 
+        buffer_temp = {'obs': mb_obs,
                        'ag': mb_ag,
-                       'g': mb_g, 
-                       'actions': mb_actions, 
+                       'g': mb_g,
+                       'actions': mb_actions,
                        'obs_next': mb_obs_next,
                        'ag_next': mb_ag_next,
                        }
         transitions = self.her_module.sample_her_transitions(buffer_temp, num_transitions)
         obs, g = transitions['obs'], transitions['g']
+        print(f'The shape of obs : {obs}')# The shape of obs : (50, 10)
+        print(f'The shape of g : {g}') # The shape of g: (50, 3)
         # pre process the obs and g
         transitions['obs'], transitions['g'] = self._preproc_og(obs, g)
         # update
@@ -256,13 +262,17 @@ class ddpg_agent:
     # soft update
     def _soft_update_target_network(self, target, source):
         for target_param, param in zip(target.parameters(), source.parameters()):
-            target_param.data.copy_((1 - self.args.sp_polyak) * param.data + self.args.sp_polyak * target_param.data)
+            target_param.data.copy_((1 - self.args.polyak) * param.data + self.args.polyak * target_param.data)
 
     # update the network
     def _update_network(self):
         # sample the episodes
         transitions = self.buffer.sample(self.args.batch_size)
-        # transitions = self.replay_buffer.sample(self.args.batch_size)
+        print(f'The Shape of obs : {transitions["obs"].shape}') # The Shape of obs : (256, 10)
+        print(f'The Shape of g: {transitions["g"].shape}') # The Shape of g: (256, 3)
+        print(f'The Shape of ag : {transitions["ag"].shape}') # The Shape of ag : (256, 3)
+        print(f'The Shape of actions: {transitions["actions"].shape}') # The Shape of actions: (256, 4)
+
         # pre-process the observation and goal
         o, o_next, g = transitions['obs'], transitions['obs_next'], transitions['g']
         transitions['obs'], transitions['g'] = self._preproc_og(o, g)
@@ -278,7 +288,7 @@ class ddpg_agent:
         inputs_norm_tensor = torch.tensor(inputs_norm, dtype=torch.float32)
         inputs_next_norm_tensor = torch.tensor(inputs_next_norm, dtype=torch.float32)
         actions_tensor = torch.tensor(transitions['actions'], dtype=torch.float32)
-        r_tensor = torch.tensor(transitions['r'], dtype=torch.float32) 
+        r_tensor = torch.tensor(transitions['r'], dtype=torch.float32)
         if self.args.cuda:
             inputs_norm_tensor = inputs_norm_tensor.cuda()
             inputs_next_norm_tensor = inputs_next_norm_tensor.cuda()
